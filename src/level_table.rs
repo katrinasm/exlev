0b6650cf953a0d07ec55d5d5074c2517bda95601
katrinasm <kittys.junk.6502@gmail.com>
2018-01-10 21:11:31 -0700

    Add level version table The next feature to add is a level extractor. In order for that to work, we need to be able to tell if levels are original SMW or if they have been modified, and if they have been modified what version of the level format they are using. This adds a table to track that in the ROM.

--- src/level_table.rs
+++ src/level_table.rs
@@ -30,17 +30,18 @@ pub fn rm_level(rombytes: &mut [u8], level: u16) -> Option<(Address, usize)> {
     if !is_rats_clnp(&rombytes[tag_start .. start]) {
         return None;
     };
-    
+
     let len = (rombytes[tag_start + 4] as usize | ((rombytes[tag_start + 5] as usize) << 8)) + 8;
-    
+
     let end = if tag_start + len > rombytes.len() { rombytes.len() } else { tag_start + len };
-    
+
     for i in tag_start .. end {
         rombytes[i] = 0;
     }
-    
+
     set_level_ptr(rombytes, level, 0);
-    
+    set_version(rombytes, level);
+
     let new_ptr = Address::new_from_pc(tag_start, Lorom).unwrap();
     Some((new_ptr, len))
 }
@@ -54,5 +55,58 @@ fn is_rats_clnp(bytes: &[u8]) -> bool {
         && b[4] == !b[6] && b[5] == !b[7]
         && b[8..12] == b"CLNP"[..]
     }
+}
+
+const VERSION_PTR_LOC: usize = 0x7f080;
+
+pub fn get_version(rombytes: &[u8], lvlnum: u16) -> (u8, u8, u8) {
+    if let Some(table_ptr) = get_version_table_ptr(rombytes) {
+        let ofs = table_ptr.pc_ofs() + (lvlnum as usize) * 3;
+        (rombytes[ofs], rombytes[ofs + 1], rombytes[ofs + 2])
+    } else {
+        (0, 0, 0)
+    }
+}
+
+pub fn set_version(rombytes: &mut [u8], lvlnum: u16) -> Result<(), String> {
+    let level_version = [0, 1, 0];
+    let table_ptr = match get_version_table_ptr(rombytes) {
+        None =>
+            init_version_table(rombytes).ok_or("no dang space for the level table")?,
+        Some(a) => a,
+    };
+
+    println!("ver table: {:08x}", table_ptr.pc_ofs());
+
+    let targ = table_ptr.pc_ofs() + (lvlnum as usize) * 3;
+    rombytes[targ] = level_version[0];
+    rombytes[targ + 1] = level_version[1];
+    rombytes[targ + 2] = level_version[2];
+    Ok(())
+}
+
+fn get_version_table_ptr(rombytes: &[u8]) -> Option<Address> {
+    let a = Address::new_from_snes_bytes(
+        &rombytes[VERSION_PTR_LOC .. VERSION_PTR_LOC + 3],
+        Lorom
+    ).unwrap();
+    if a.snes_ofs().unwrap() == 0xff_ffff {
+        None
+    } else {
+        Some(a)
+    }
+}
+
+fn init_version_table(rombytes: &mut [u8]) -> Option<Address> {
+    let default = &[0; 512 * 3];
+    let a = match ::rats::insert_free(rombytes, default) {
+        None => return None,
+        Some(a) => a,
+    };
+    let ofs = a.snes_ofs().unwrap();
+    rombytes[VERSION_PTR_LOC] = ofs as u8;
+    rombytes[VERSION_PTR_LOC + 1] = (ofs >> 8) as u8;
+    rombytes[VERSION_PTR_LOC + 2] = (ofs >> 16) as u8;
+    Some(a)
 }
 
